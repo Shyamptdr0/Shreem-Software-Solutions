b159ca29d3f80b9f30bf5a55494828cb767f96ec
Shyam Patidar <anu.shyam.patidar@gmail.com>
2025-12-07 17:02:37 +0530

    update new change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
 	siteUrl: 'https://shreemsoftwaresolutions.com',
-	generateRobotsTxt: true, // (optional)
-	sitemapSize: 5000, // if you have a lot of pages
+	generateRobotsTxt: true,
+	sitemapSize: 5000,
+	changefreq: 'weekly',
+	priority: 0.8,
+	exclude: ['/admin/*'], // optional
 };
